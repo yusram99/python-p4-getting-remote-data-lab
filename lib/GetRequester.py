1a59f67229cae54ad902172cdc424675d6c21688
Your Name <youremail@example.com>
2023-09-21 12:33:19 +0300

    commit message

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,10 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
-
+        URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+        
+        response = requests.get(URL)
+        return response.content
+    
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
